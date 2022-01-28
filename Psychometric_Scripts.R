8ee1f4aed23d1c31f942d7eaf9b63f379cc19774
zenit125 <robertchapman125@gmail.com>
2022-01-28 06:47:41 -0600

    Update Psychometric_Scripts.R

--- Psychometric_Scripts.R
+++ Psychometric_Scripts.R
@@ -29,13 +29,13 @@ ceiling_list=function(data, x, max=NA, collapsed=NA){return(sapply(x, simplify=T
 	round(sum(data[,y] %in% max)/length(data[,y]),2)}))}
 
 item_change_vector=function(data){
-	apply(data, 1, function(x){
-		temp=data
-		temp[is.na(temp)]=0
-		temp=diff(as.numeric(temp)) #has problems with numeric??
-		temp[unique(c(which(is.na(data[-1])), which(is.na(data[-1]))+1))]=NA
-		temp
-		})}
+	  apply(data, 1, function(x){
+	    temp=as.matrix(data)
+	    temp[is.na(temp)]=0
+	    temp=diff(temp) 
+	    temp[unique(c(which(is.na(data[-1])), which(is.na(data[-1]))+1))]=NA
+	    temp
+	  })}
 
 Outlier_n=function(data,x){sapply(x, function(y){z=scale(data[,y]);return(sum(which(abs(z)>3.29)));})}
 
